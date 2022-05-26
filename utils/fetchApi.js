1ade95e97df3a9d1d01c81c25029f137fa8870a3
Victor Bustamante Cera <victorbust.c@gmail.com>
2022-05-26 18:19:39 -0400

    updating fetch api
    updating fetch api

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -1,15 +1,14 @@
 import axios from "axios";
 
-export const baseUrl ="https://bayut.p.rapidapi.com"
+export const baseUrl = "https://bayut.p.rapidapi.com";
 
+export const fetchApi = async (url) => {
+  const { data } = await axios.get(url, {
+    headers: {
+      "X-RapidAPI-Host": "bayut.p.rapidapi.com",
+      "X-RapidAPI-Key": "7208515210msh736003e7a105ec2p1cb134jsn81ee96c65827",
+    },
+  });
 
-export const fetchApi = async(url) =>{
-    const {data} = await axios.get((url), {
-        headers: {
-            'X-RapidAPI-Host': 'bayut.p.rapidapi.com',
-            'X-RapidAPI-Key': 'e2471dfb99mshd883609e942ef11p130145jsn372697a05be7'
-        }
-    })
-
-    return data
-}
+  return data;
+};
